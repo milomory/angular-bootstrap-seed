38777368d44771707309d292b7e8d2eef21141c3
kollors <kollors@mail.ru>
2016-09-21 11:29:11 +0300

    0.0.1 add font-awesome

--- sources/vendor.js
+++ sources/vendor.js
@@ -5,6 +5,8 @@
 require('jquery');
 require('bootstrap');
 require('bootstrap/dist/css/bootstrap.css');
+require('font-awesome/css/font-awesome.css');
+require('awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css');
 
 require('angular');
 
