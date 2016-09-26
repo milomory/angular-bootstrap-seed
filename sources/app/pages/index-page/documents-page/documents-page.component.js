1f05f99055db958a32d5f7d01fe5ae795ffa404a
kollors <kollors@mail.ru>
2016-09-26 14:20:07 +0300

    0.0.3 tags select

--- sources/app/pages/index-page/documents-page/documents-page.component.js
+++ sources/app/pages/index-page/documents-page/documents-page.component.js
@@ -5,7 +5,7 @@
 angular.module('app').config($stateProvider => {
     $stateProvider.state({
         name: 'index.documents',
-        url: '/documents?page&limit&name&creatorId&scopes',
+        url: '/documents?page&limit&name&creatorId&tagIds&scopes',
         params: {
             page: {value: '1', squash: true},
             limit: {value: '10', squash: true},
@@ -15,31 +15,47 @@ angular.module('app').config($stateProvider => {
         component: 'documentsPage',
         resolve: {
             documents: ($stateParams, apiService) => apiService.Document.query($stateParams).$promise,
-            users: apiService => apiService.User.query().$promise
+            users: apiService => apiService.User.query().$promise,
+            tags: apiService => apiService.Tag.query().$promise
         }
     });
 }).component('documentsPage', {
     bindings: {
         documents: '<',
-        users: '<'
+        users: '<',
+        tags: '<'
     },
     template: require('./documents-page.component.html'),
     controller: function ($cookies, $state, appConfig, modalService) {
         this.currentUser = $cookies.getObject('currentUser');
-        this.params = $state.params;
+        this.params = angular.copy($state.params);
+        this.params.tagIds = this.params.tagIds.split(',').map(tagId => ({id: tagId}));
         this.itemsPerPage = appConfig.itemsPerPage;
 
         this.queryDocuments = params => {
-            angular.extend(this.params, params);
-            this.documents.$query(this.params).then(documents => {
+            if (params.hasOwnProperty('tagIds')) {
+                params.tagIds = params.tagIds.map(tag => tag.id).join(',');
+            }
+
+            angular.extend($state.params, params);
+
+            this.documents.$query($state.params).then(documents => {
                 this.documents = documents;
-                $state.go('.', this.params);
+                $state.go('.', $state.params);
             });
         };
 
         this.showDocumentModal = documentId => {
             modalService.showDocumentModal(documentId).then(document => {
                 this.queryDocuments();
+            });
+        };
+
+        this.queryTags = params => {
+            if (!params.name) return;
+
+            this.tags.$query(params).then(tags => {
+                this.tags = tags;
             });
         };
     }
